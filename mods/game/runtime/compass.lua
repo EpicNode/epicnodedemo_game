1cdb90f5acaaa109d66a0f8bd4164bb96025d0c7
Pinky <thepinkysnowman@gmail.com>
2016-01-18 21:33:30 -0500

    :D

--- mods/game/runtime/compass.lua
+++ mods/game/runtime/compass.lua
@@ -15,7 +15,7 @@ minetest.register_node("game:compass", {
 		local meta = minetest.get_meta(pos);
 		local pos_s = minetest.pos_to_string({x=math.floor(pos.x),y=math.floor(pos.y),z=math.floor(pos.z)})
 		local place = ""
-		local _, _1, _2, place = places.get_closest_string(pos)
+		local _, _1, _2, place = game.get_closest_string(pos)
 		meta:set_string("infotext", "\t Location\n\t"..pos_s.."\n"..place)
 	end,
 })
